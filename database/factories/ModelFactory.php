acf3c40730f793f7753a1b0ae2c9dd79000f8759
Penthious <tleffew1994@gmail.com>
2017-01-03 22:53:14 -0700

    refactored code to make it more readable

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -12,30 +12,43 @@
 */
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
+use App\Concert;
 use Carbon\Carbon;
 
-$factory->define(App\User::class, function ( Faker\Generator $faker) {
+$factory->define(App\User::class, function ( Faker\Generator $faker ) {
     static $password;
 
     return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'password' => $password ?: $password = bcrypt('secret'),
+        'name'           => $faker->name,
+        'email'          => $faker->unique()->safeEmail,
+        'password'       => $password ?: $password = bcrypt('secret'),
         'remember_token' => str_random(10),
     ];
 });
 
-$factory->define(App\Concert::class, function (Faker\Generator $faker){
+$factory->define(Concert::class, function ( Faker\Generator $faker ) {
+    return [
+        'title'                  => 'Example Band',
+        'subtitle'               => 'with the Fake Openers',
+        'date'                   => Carbon::parse('+2 weeks'),
+        'ticket_price'           => 2000,
+        'venue'                  => 'The Example Theatre',
+        'venue_address'          => '123 Example Lane',
+        'city'                   => 'Fakeville',
+        'state'                  => 'ON',
+        'zip'                    => '90210',
+        'additional_information' => 'Some sample additional information.',
+    ];
+});
+
+$factory->state(Concert::class, 'published', function ( $faker ) {
    return [
-       'title'                 => 'Example Band',
-       'subtitle'              => 'with the Fake Openers',
-       'date'                  => Carbon::parse('+2 weeks'),
-       'ticket_price'          => 2000,
-       'venue'                 => 'The Example Theatre',
-       'venue_address'         => '123 Example Lane',
-       'city'                  => 'Fakeville',
-       'state'                 => 'ON',
-       'zip'                   => '90210',
-       'additional_information' => 'Some sample additional information.',
-   ];
+       'published_at' => Carbon::parse('-1 week'),
+   ] ;
+});
+
+$factory->state(Concert::class, 'unpublished', function ( $faker ) {
+    return [
+        'published_at' => null,
+    ] ;
 });
